ab53b88a559253be42e9a224b8b39b7a927cce97
takaki <t.takaki7255@gmail.com>
2024-04-06 10:07:02 +0900

    initial commit

---
+++ youtube_downloader.py
@@ -0,0 +1,15 @@
+from yt_dlp import YoutubeDL
+
+# ユーザーにYouTubeビデオのURLを入力するよう求める
+video_url = input("ダウンロードするYouTubeビデオのURLを入力してください: ")
+
+# ydl_optsでダウンロードオプションを設定
+ydl_opts = {
+    "outtmpl": "downloads/%(title)s.%(ext)s",  # ダウンロード先のディレクトリとファイル名
+    "format": "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best",  # 動画と音声の品質を指定
+}
+
+# ダウンローダーの作成
+with YoutubeDL(ydl_opts) as ydl:
+    # ダウンロード
+    ydl.download([video_url])
