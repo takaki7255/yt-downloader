99b26fee5b49b1cf58885d1528517ca2ab93cbf5
takaki <t.takaki7255@gmail.com>
2025-08-25 18:05:53 +0900

    .gitignoreを追加し、YouTubeビデオのURL入力プロンプトを修正

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -1,12 +1,11 @@
 from yt_dlp import YoutubeDL
 
 # ユーザーにYouTubeビデオのURLを入力するよう求める
-video_url = input("ダウンロードするYouTubeビデオのURLを入力してください: ")
+video_url = input("ダウンロードするYouTubeビデオのURLを入力してください\n")
 
 # ydl_optsでダウンロードオプションを設定
 ydl_opts = {
     "outtmpl": "downloads/%(title)s.%(ext)s",  # ダウンロード先のディレクトリとファイル名
-    "format": "best",  # 動画と音声の品質を指定
 }
 
 # ダウンローダーの作成
